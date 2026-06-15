dd68fa12c467ea12060191eaee2af8ab0031425a
binlongZhang <binlong_zhang@163.com>
2026-06-15 19:57:19 +0800

    调整部署配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://binlongzhang.github.io',
+	base: '/personalBlog',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
